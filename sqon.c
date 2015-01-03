acdb07893ba4420f4ef74099fe8165fe0101ad7c
David McMackins II <contact@mcmackins.org>
2015-01-03 08:38:12 -0600

    Implement basic MySQL database connectivity

--- sqon.c
+++ sqon.c
@@ -16,6 +16,7 @@
  */
 
 #include <jansson.h>
+#include <mysql.h>
 
 #include "sqon.h"
 #include "safemem.h"
@@ -26,4 +27,46 @@ void sqon_init(size_t qlen)
 {
     QLEN = qlen;
     json_set_alloc_funcs(safe_malloc, safe_free);
+}
+
+struct dbconn sqon_new_connection(uint8_t type, const char *host,
+        const char *user, const char *passwd, const char *database)
+{
+    struct dbconn out = {
+        .isopen = false,
+        .type = type,
+        .host = host,
+        .user = user,
+        .passwd = passwd,
+        .database = database
+    };
+    return out;
+}
+
+int sqon_connect(struct dbconn *con)
+{
+    switch (con->type) {
+        case SQON_DBCONN_MYSQL:
+            con->con = mysql_init(NULL);
+            if (NULL == mysql_real_connect(con->con, con->host, con->user,
+                    con->passwd, con->database, 0, NULL, 0))
+                return (int) mysql_errno(con->con);
+            break;
+
+        default:
+            return -1;
+    }
+
+    con->isopen = true;
+    return 0;
+}
+
+void sqon_close(struct dbconn *con)
+{
+    switch (con->type) {
+        case SQON_DBCONN_MYSQL:
+            mysql_close(con->con);
+            con->isopen = false;
+            break;
+    }
 }
--- sqon.h
+++ sqon.h
@@ -26,6 +26,9 @@
 #ifndef DELWINK_SQON_H
 #define DELWINK_SQON_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 /**
  * @brief libsqon software version
  */
@@ -34,7 +37,7 @@
 /**
  * @brief Information about the libsqon copyright holders and license.
  */
-#define SQON_COPYRIGHT\
+#define SQON_COPYRIGHT \
 "libsqon - C API for Delwink's Structured Query Object Notation\n"\
 "Copyright (C) 2015 Delwink, LLC\n\n"\
 "This program is free software: you can redistribute it and/or modify\n"\
@@ -57,6 +60,49 @@ extern "C"
  * @param qlen Maximum length for queries to the database.
  */
 void sqon_init(size_t qlen);
+
+/**
+ * @brief The universal database connection auxiliary structure for libsqon.
+ */
+struct dbconn {
+    void *con;
+    bool isopen;
+    uint8_t type;
+    const char *host;
+    const char *user;
+    const char *passwd;
+    const char *database;
+};
+
+/**
+ * @brief Connection type constant for MySQL-based databases.
+ */
+#define SQON_DBCONN_MYSQL 1
+
+/**
+ * @brief Constructs a database connection.
+ * @param type Connection type constant, such as SQON_DBCONN_MYSQL.
+ * @param host The hostname or IP address of the database server.
+ * @param user The username with which to authenticate with the database server.
+ * @param passwd The password by which to be authenticated.
+ * @param database The name of the database to be used; can be NULL.
+ * @return A new database connection object.
+ */
+struct dbconn sqon_new_connection(uint8_t type, const char *host,
+        const char *user, const char *passwd, const char *database);
+
+/**
+ * @brief Connects to the database server.
+ * @param con Initialized database connection object.
+ * @return Negative if input error; positive if connection error.
+ */
+int sqon_connect(struct dbconn *con);
+
+/**
+ * @brief Closes connection to the database server.
+ * @param con Connected database connection object.
+ */
+void sqon_close(struct dbconn *con);
 
 #ifdef __cplusplus
 }
