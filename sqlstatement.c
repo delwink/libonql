f20b4485480f7dc38e208af93597285559c0c50e
David McMackins II <contact@mcmackins.org>
2015-01-21 18:26:30 -0600

    Fix license header

--- sqlstatement.c
+++ sqlstatement.c
@@ -1,4 +1,7 @@
 /*
+ *  Structured Query Object Notation (SQON) - C API
+ *  Copyright (C) 2015 Delwink, LLC
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
  *  the Free Software Foundation, version 3 only.
