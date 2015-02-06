44a6ee378da16e71a10680aebbab4bf23ba64931
David McMackins II <contact@mcmackins.org>
2015-02-05 18:19:03 -0600

    Cleaned up conditions

--- sqlcondition.c
+++ sqlcondition.c
@@ -22,34 +22,67 @@
 #include "sqlquery.h"
 #include "sqlcondition.h"
 
-int
-equal (sqon_dbsrv *srv, json_t *in, char *out, size_t n, const char *sep,
-       bool space)
+#define VALUE_UNIT    1
+#define VALUE_CSV     2
+#define VALUE_BETWEEN 3
+
+static int
+write_condition (const char *col, const char *split, const char *val,
+		 char *temp, char *out, size_t n)
 {
-  json_incref (in);
+  size_t written = strlen (out);
+  int to_write;
+
+  if (written >= n)
+    return SQON_OVERFLOW;
+
+  to_write = snprintf (temp, n, "%s%s%s", col, split, val);
+
+  written += to_write;
+  if ((size_t) to_write >= n || written >= n)
+    return SQON_OVERFLOW;
+
+  strcat (out, temp);
+  return 0;
+}
+
+static int
+separate_condition (const char *sep, const char *buffer, char *out, size_t n)
+{
+  size_t blen = strlen (buffer), seplen = strlen (sep);
+  size_t written = strlen (out) + (2 * blen) + seplen;
+
+  if (written >= n)
+    return SQON_OVERFLOW;
+
+  strcat (out, buffer);
+  strcat (out, sep);
+  strcat (out, buffer);
+
+  return 0;
+}
+
+static int
+abstract_condition (sqon_dbsrv *srv, json_t *in, char *out, size_t n,
+		    const char *sep, bool space, const char *split,
+		    uint8_t val_type)
+{
   int rc = 0;
   const char *key, *buffer = space ? sp : "";
-  size_t written = 1, blen = strlen (buffer);
   json_t *value;
+  size_t left;
   char *col, *val, *temp;
 
   if (!json_is_object (in))
-    {
-      json_decref (in);
-      return SQON_TYPEERROR;
-    }
+    return SQON_TYPEERROR;
 
   col = sqon_malloc (n * sizeof (char));
   if (NULL == col)
-    {
-      json_decref (in);
-      return SQON_MEMORYERROR;
-    }
+    return SQON_MEMORYERROR;
 
   val = sqon_malloc (n * sizeof (char));
   if (NULL == val)
     {
-      json_decref (in);
       sqon_free (col);
       return SQON_MEMORYERROR;
     }
@@ -57,248 +90,73 @@ equal (sqon_dbsrv *srv, json_t *in, char *out, size_t n, const char *sep,
   temp = sqon_malloc (n * sizeof (char));
   if (NULL == temp)
     {
-      json_decref (in);
-      sqon_free (col);
       sqon_free (val);
+      sqon_free (col);
       return SQON_MEMORYERROR;
     }
 
   *out = '\0';
-  size_t seplen = strlen (sep);
-  size_t left = json_object_size (in);
+  left = json_object_size (in);
 
   json_object_foreach (in, key, value)
     {
       rc = escape (srv, key, col, n, false);
       if (rc)
 	break;
-      rc = json_to_sql_type (srv, value, val, n, true);
+
+      switch (val_type)
+	{
+	case VALUE_UNIT:
+	  rc = json_to_sql_type (srv, value, val, n, true);
+	  break;
+
+	case VALUE_CSV:
+	  rc = json_to_csv (srv, value, val, n, true);
+	  break;
+
+	default:
+	  rc = SQON_UNSUPPORTED;
+	  break;
+	}
       if (rc)
 	break;
 
-      rc = snprintf (temp, n, "%s=%s", col, val);
-      if ((size_t) rc >= n)
-	{
-	  rc = SQON_OVERFLOW;
-	  break;
-	}
-      else
-	{
-	  written += rc;
-	  rc = 0;
-	}
-
-      strcat (out, temp);
+      rc = write_condition (col, split, val, temp, out, n);
+      if (rc)
+	break;
 
       if (--left > 0)
-	{
-	  written += (blen * 2) + seplen;
-	  if (written < n)
-	    {
-	      strcat (out, buffer);
-	      strcat (out, sep);
-	      strcat (out, buffer);
-	    }
-	  else
-	    {
-	      rc = SQON_OVERFLOW;
-	      break;
-	    }
-	}
+	rc = separate_condition (sep, buffer, out, n);
+      if (rc)
+	break;
     }
 
   sqon_free (temp);
-  sqon_free (col);
   sqon_free (val);
-  json_decref (in);
+  sqon_free (col);
   return rc;
+}
+
+int
+equal (sqon_dbsrv *srv, json_t *in, char *out, size_t n, const char *sep,
+       bool space)
+{
+  return abstract_condition (srv, in, out, n, sep, space, "=", VALUE_UNIT);
 }
 
 static int
 in_cond (sqon_dbsrv *srv, json_t *in, char *out, size_t n, const char *sep,
 	 bool space)
 {
-  json_incref (in);
-  int rc = 0;
-  const char *key, *buffer = space ? sp : "";
-  size_t written = 1, blen = strlen (buffer);
-  json_t *value;
-  char *col, *vals, *temp;
-
-  if (!json_is_object (in))
-    {
-      json_decref (in);
-      return SQON_TYPEERROR;
-    }
-
-  col = sqon_malloc (n * sizeof (char));
-  if (NULL == col)
-    {
-      json_decref (in);
-      return SQON_MEMORYERROR;
-    }
-
-  vals = sqon_malloc (n * sizeof (char));
-  if (NULL == vals)
-    {
-      sqon_free (col);
-      json_decref (in);
-      return SQON_MEMORYERROR;
-    }
-
-  temp = sqon_malloc (n * sizeof (char));
-  if (NULL == temp)
-    {
-      sqon_free (vals);
-      sqon_free (col);
-      json_decref (in);
-      return SQON_MEMORYERROR;
-    }
-
-  *out = '\0';
-  size_t seplen = strlen (sep);
-  size_t left = json_object_size (in);
-
-  json_object_foreach (in, key, value)
-    {
-      rc = escape (srv, key, col, n, false);
-      if (rc)
-	break;
-      rc = json_to_csv (srv, value, vals, n, true);
-      if (rc)
-	break;
-
-      rc = snprintf (temp, n, "%s IN (%s)", col, vals);
-      if ((size_t) rc >= n)
-	{
-	  rc = SQON_OVERFLOW;
-	  break;
-	}
-      else
-	{
-	  written += rc;
-	  rc = 0;
-	}
-
-      strcat (out, temp);
-
-      if (--left > 0)
-	{
-	  written += (blen * 2) + seplen;
-	  if (written < n)
-	    {
-	      strcat (out, buffer);
-	      strcat (out, sep);
-	      strcat (out, buffer);
-	    }
-	  else
-	    {
-	      rc = SQON_OVERFLOW;
-	      break;
-	    }
-	}
-    }
-
-  sqon_free (vals);
-  sqon_free (col);
-  sqon_free (temp);
-  json_decref (in);
-  return rc;
+  return abstract_condition (srv, in, out, n, sep, space, " IN ", VALUE_CSV);
 }
 
 static int
 like (sqon_dbsrv *srv, json_t *in, char *out, size_t n, const char *sep,
       bool space)
 {
-  json_incref (in);
-  int rc = 0;
-  const char *key, *buffer = space ? sp : "";
-  size_t written = 1, blen = strlen (buffer);
-  json_t *value;
-  char *col, *val, *temp;
-
-  if (!json_is_object (in))
-    {
-      json_decref (in);
-      return SQON_TYPEERROR;
-    }
-
-  col = sqon_malloc (n * sizeof (char));
-  if (NULL == col)
-    {
-      json_decref (in);
-      return SQON_MEMORYERROR;
-    }
-
-  val = sqon_malloc (n * sizeof (char));
-  if (NULL == val)
-    {
-      sqon_free (col);
-      json_decref (in);
-      return SQON_MEMORYERROR;
-    }
-
-  temp = sqon_malloc (n * sizeof (char));
-  if (NULL == temp)
-    {
-      sqon_free (val);
-      sqon_free (col);
-      json_decref (in);
-      return SQON_MEMORYERROR;
-    }
-
-  *out = '\0';
-  size_t seplen = strlen (sep);
-  size_t left = json_object_size (in);
-
-  json_object_foreach (in, key, value)
-    {
-      if (!json_is_string (value))
-	{
-	  rc = SQON_TYPEERROR;
-	  break;
-	}
-
-      rc = escape (srv, key, col, n, false);
-      if (rc)
-	break;
-
-      rc = snprintf (temp, n, "%s LIKE %s", col, json_string_value (value));
-      if ((size_t) rc >= n)
-	{
-	  rc = SQON_OVERFLOW;
-	  break;
-	}
-      else
-	{
-	  written += rc;
-	  rc = 0;
-	}
-
-      strcat (out, temp);
-
-      if (--left > 0)
-	{
-	  written += (blen * 2) + seplen;
-	  if (written < n)
-	    {
-	      strcat (out, buffer);
-	      strcat (out, sep);
-	      strcat (out, buffer);
-	    }
-	  else
-	    {
-	      rc = SQON_OVERFLOW;
-	      break;
-	    }
-	}
-    }
-
-  sqon_free (temp);
-  sqon_free (val);
-  sqon_free (col);
-  json_decref (in);
-  return rc;
+  return abstract_condition (srv, in, out, n, sep, space, " LIKE ",
+			     VALUE_UNIT);
 }
 
 int
